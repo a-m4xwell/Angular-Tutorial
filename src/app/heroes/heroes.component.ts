18fa372a8b35ab38a4c3e5bce030ae7bb3c42760
Andrew Maxwell <Andrew.Maxwell@healthcaregateway.co.uk>
2022-08-03 09:01:01 +0100

    New components

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  selectHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
